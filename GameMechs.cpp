58732a5f3bdf4242c748ee09d7d06b302aabade5
Makarios Samwail <samwailm@mcmaster.ca>
2023-11-29 23:32:11 -0500

    Added comments to finish of iteration 1B

--- GameMechs.cpp
+++ GameMechs.cpp
@@ -3,6 +3,7 @@
 
 GameMechs::GameMechs()
 {
+    //default constructor
     boardSizeX = 30;
     boardSizeY = 15;
     exitFlag = false;
@@ -13,6 +14,7 @@ GameMechs::GameMechs()
 
 GameMechs::GameMechs(int boardX, int boardY)
 {
+    //special constructor which allows programmer to set the x and y size of the board
     boardSizeX = boardX;
     boardSizeY = boardY;
     exitFlag = false;
@@ -27,67 +29,80 @@ GameMechs::GameMechs(int boardX, int boardY)
 
 bool GameMechs::getExitFlagStatus()
 {
+    //returns exit flag
     return exitFlag;
 }
 
 void GameMechs::setExitTrue()
 {
+    //sets exit flag to true which is used to break out of the program loop and print the exit message
     exitFlag = true;
 }
 
 bool GameMechs::getLoseFlagStatus()
 {
+    //returens the lose flag
     return loseFlag;
 }
 
 void GameMechs::setLoseFlag()
 {
+    //sets exit flag to true which is used to break out of the program loop and print the losing message
     loseFlag = true;
 }
 
 void GameMechs::readInput()
 {
+    //checks if user has made a keyboard input and set the input attribute equal to that char
+    //if there is no keyboard input then set the attribute to null
     if (MacUILib_hasChar())
     {
         setInput(MacUILib_getChar());
     }
     else
     {
-        setInput(0);
+        clearInput();
     }
 }
 
 char GameMechs::getInput()
 {
+    //returns input attribute
     return input;
 }
 
 void GameMechs::setInput(char this_input)
 {
+    //sets the input attribute to the char passed into this method
     input = this_input;
 }
 
 void GameMechs::clearInput()
 {
+    //sets input to 0
     input = 0;
 }
 
 int GameMechs::getBoardSizeX()
 {
+    //returns the horizontal size of the board
     return boardSizeX;
 }
 
 int GameMechs::getBoardSizeY()
 {
+    //returns the vertical size of the board
     return boardSizeY;
 }
 
 int GameMechs::getScore()
 {
+    //returns the score
     return score;
 }
 
 void GameMechs::incrementScore()
 {
+    //increments the score
     score ++;
 }
--- Project.cpp
+++ Project.cpp
@@ -42,26 +42,26 @@ void Initialize(void)
     MacUILib_init();
     MacUILib_clearScreen();
 
-    gameMechs = new GameMechs(BOARD_WIDTH, BOARD_HEIGHT);
+    gameMechs = new GameMechs(BOARD_WIDTH, BOARD_HEIGHT); //create a GameMechs object with a specified board size on the heap
     player = new Player(gameMechs);
 
 }
 
 void GetInput(void)
 {
-    gameMechs->readInput();
+    gameMechs->readInput(); //get the users input and store it in the gameMechs input attribute
 }
 
 void RunLogic(void)
 {
     if(gameMechs->getInput() == 27)
     {
-        gameMechs->setExitTrue();
+        gameMechs->setExitTrue(); //set the exit flag true if the users input is the esc key
     }
     if(gameMechs->getInput() == 'p')
-        gameMechs->incrementScore();
+        gameMechs->incrementScore(); //increment the gameMechs score attribute by one using the p key
     if(gameMechs->getInput() == 'o')
-        gameMechs->setLoseFlag();
+        gameMechs->setLoseFlag(); //set the loseFlag attribute to true with the o key
 
     player->updatePlayerDir();
     player->movePlayer();
@@ -73,7 +73,7 @@ void DrawScreen(void)
     objPos curr_player_pos;
     player->getPlayerPos(curr_player_pos);
 
-    objPos game_ui[gameMechs->getBoardSizeY()][gameMechs->getBoardSizeX()]; //Let marwan know that we should be using gameMechs object for initializing the board
+    objPos game_ui[gameMechs->getBoardSizeY()][gameMechs->getBoardSizeX()];
     
     // Initialize the border and inner spaces in one loop
     for (int i = 0; i < gameMechs->getBoardSizeY(); i++)
@@ -99,7 +99,7 @@ void DrawScreen(void)
         MacUILib_printf("\n");
     }
 
-    MacUILib_printf("score is %d\n", gameMechs->getScore());
+    MacUILib_printf("score is %d\n", gameMechs->getScore()); //print the users score at the bottom
     
     
 
@@ -116,9 +116,9 @@ void CleanUp(void)
 {
     MacUILib_clearScreen();
     if(gameMechs->getLoseFlagStatus() == true)
-        MacUILib_printf("You lost. Final score is %d\n", gameMechs->getScore());
+        MacUILib_printf("You lost. Final score is %d\n", gameMechs->getScore()); //print the final score if the lose flag is set true
     else if(gameMechs->getExitFlagStatus() == true)
-        MacUILib_printf("You have successfully quits");
+        MacUILib_printf("You have successfully quits"); //print a successful quit message if the exit flag is true
     MacUILib_uninit();
 
     delete gameMechs;
